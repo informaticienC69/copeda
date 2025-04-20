d5469c69f989fdfa8391f403d8e7302dd2d7209b
informaticienC69 <tekaloga72@gmail.com>
2025-04-20 17:34:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 document.getElementById('form').addEventListener('submit', function(e) {
   e.preventDefault();
   const formData = new FormData(this);
-  const data = {};
-  formData.forEach((value, key) => data[key] = value);
-fetch("https://script.google.com/macros/s/AKfycbyHqsb46_tHA_lFJOYxRRvmVRj0nBGRLc1qeAWt2ZWWS3vwigif_KXaHKuNNjI1AImxpQ/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbyHqsb46_tHA_lFJOYxRRvmVRj0nBGRLc1qeAWt2ZWWS3vwigif_KXaHKuNNjI1AImxpQ/exec", {
     method: "POST",
-    body: JSON.stringify(data)
-  }).then(res => res.json()).then(res => {
+    body: formData
+  })
+  .then(res => res.text())
+  .then(res => {
     alert("Enregistrement réussi !");
     document.getElementById('form').reset();
+  })
+  .catch(err => {
+    alert("Une erreur est survenue !");
+    console.error(err);
   });
 });
