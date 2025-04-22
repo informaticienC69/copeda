9a447c075d2179c67a008f403ce0f334226a8fa9
informaticienC69 <tekaloga72@gmail.com>
2025-04-22 01:30:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://script.google.com/macros/s/AKfycbxzEdfKHmIGzFcTtLu1wl-1Zvvz1gvc20ZqBht7IKmTVFzboXMJ0clkmlQQqE9ulDln/exec';
+const BASE_URL = 'https://script.google.com/macros/s/AKfycbwlCxO18zSatXsGDLPCPWYu-KxIUYcVC7S8RUpW_FiuBCfA-eQ8J_LqPH_rWxcY7tTppA/exec';
 
 document.addEventListener('DOMContentLoaded', () => {
   const typeSelect = document.getElementById('type');
